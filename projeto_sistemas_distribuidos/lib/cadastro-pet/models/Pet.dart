10a1ea90c3d106102fef54eff8fc5f0f7aeb6034
unknown <vinicius.soaresvc@gmail.com>
2022-05-03 00:19:40 -0300

    alterações service para API

--- projeto_sistemas_distribuidos/lib/cadastro-pet/models/Pet.dart
+++ projeto_sistemas_distribuidos/lib/cadastro-pet/models/Pet.dart
@@ -1,18 +1,35 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 class Pet {
   String? nome;
   String? raca;
   String? localizacao;
-  Uint8List? imagem;
+  String? imagem;
+  int? idade;
+  int? codigo;
 
-  Pet({this.nome, this.raca, this.localizacao, this.imagem});
+  Pet({this.nome, this.raca, this.localizacao, this.imagem, this.idade,this.codigo});
 
   static Pet fromJson(Map<String, dynamic> json) {
     return new Pet(
-        nome: json['nome'],
-        raca: json['raca'],
-        localizacao: json['localizacao'],
-        imagem: json['imagem']);
+      nome: json['nome'],
+      raca: json['raca'],
+      localizacao: json['localizacao'],
+      imagem: json['imagem'],
+      idade: json['idade'],
+      codigo: json['codigo']
+    );
+  }
+
+  Map toMap() {
+    return {
+      "nome": this.nome,
+      "raca": this.raca,
+      "localizacao": this.localizacao,
+      "imagem": this.imagem,
+      "idade": this.idade,
+      "codigo":this.codigo
+    };
   }
 }
